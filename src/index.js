e7624c068a638b2ce34bed372c3351dd9d606cff
ajay2507 <pajay2507@gmail.com>
2018-07-15 18:34:30 -0700

    added addNodeToTree method and lasso sample html

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 const fs = require('fs');
 const path = require('path');
+const Tree = require('./tree');
+const borderX = `${Array(30).join('-')}\n`;
 
-const readFile = fs.readFileSync(path.resolve('stats.json'), 'utf8');
-const readJSON = JSON.parse(readFile);
 
-// iterate through the JSON file.
-console.log(readJSON);
-for (source in readJSON) {
-    console.log(source);
+
+
+
+function bundleAnalyzer(fileName) {
+    // load lasso-unpack and create lasso-stats.json
+    const log = `${borderX}` + `Lasso Bundle Analyzer \n` +
+        `${borderX}`;
+    console.log(log);
+    const readFile = fs.readFileSync(path.resolve(fileName), 'utf8');
+    const readJSON = JSON.parse(readFile);
+    const tree = new Tree('/');
+    readJSON.forEach((source) => {
+        tree.addNodeToTree(source, tree);
+    });
+
+    console.log(tree);
 }
 
+bundleAnalyzer('stats.json')
+
+
+
+// https://stackoverflow.com/a/18650828/388951
+function formatBytes(bytes, decimals) {
+    if (bytes == 0) return '0 B';
+    let k = 1000,
+        dm = decimals || 2,
+        sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+        i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
 
--- src/tree.js
+++ src/tree.js
@@ -1,19 +1,49 @@
 // Inspired from https://github.com/evmar/webtreemap/blob/master/tree.ts
 
-class Node {
+class Tree {
     constructor(options) {
-        this.packageName = '';
-        this.size = '';
-        this.children = [];
+        this.packageName = options.packageName || '';
+        //https://github.com/evmar/webtreemap/blob/master/oldapi.ts#L10
+        this.size = {
+            '$area': options.size || 0
+        };
+        this.children = options.children || [];
+        this.path = options.path || '/';
     }
 
     setPackageName(name) {
         this.packageName = name;
     }
     setSize(size) {
-        this.size = size;
+        this.size.$area = size;
     }
     setChildren(children) {
-        this.children = children;
+        this.children.push(children);
     }
-}
+    setPath(path) {
+        this.path = path;
+    }
+    getSize() {
+        return this.size.$area;
+    }
+    // add new nodes 
+    addNodeToTree(source, tree) {
+        console.log("inside add node" + source.path);
+        const parts = source.path.split('/');
+        let node = tree;
+        const nodeSize = node.getSize();
+        node.setPackageName(source.packageName);
+        node.setSize(nodeSize + source.size);
+        parts.forEach(function (part) {
+            var child = node.children.find(function (child) { return child.name == part; });
+            if (!child) {
+                var child = new Tree(part);
+                node.children.push(child);
+            }
+            node = new Tree(child);
+        });
+        //console.log(tree);
+    };
+};
+
+module.exports = Tree;
