61165a37908f4c6d363a6626ee32c96eb663f06c
ajay2507 <pajay2507@gmail.com>
2018-10-06 13:44:03 -0700

    create bundle using bundle-me

--- bin/index.js
+++ bin/index.js
@@ -3,14 +3,15 @@
 
 const parseArgs = require('minimist');
 const lassoAnalyzer = require('../src/index');
-
+const bundleMe = require('bundle-me');
 const argv = parseArgs(process.argv.slice(2));
 const borderX = `${Array(30).join('-')}\n`;
 const input = argv._ || [];
 
 if (input.length > 0) {
     input.map((fileName) => {
-        lassoAnalyzer(fileName);
+        bundleMe({ path: fileName, outputPath: 'lasso-analyze.js' }).createBundle;
+        lassoAnalyzer('lasso-analyze.js');
     });
     const startLog = `${borderX}` + `lasso-analyze.html is created \n` +
         `${borderX}`;
