cef921504db17689d20dcc9a4a77e3b2bd81ace1
ajay2507 <pajay2507@gmail.com>
2020-06-24 01:14:54 -0700

    added better logging for cli

--- bin/index.js
+++ bin/index.js
@@ -2,27 +2,35 @@
 'use strict';
 
 const parseArgs = require('minimist');
+const Logger = require('log-cli');
 const lassoAnalyzer = require('../src/index');
 const { createBundle } = require('bundle-me');
 const argv = parseArgs(process.argv.slice(2));
-const borderX = `${Array(30).join('-')}\n`;
 // extract input file.
 const input = argv._ || [];
 // extract output file.
 let outputFile = parseArgs(process.argv).output;
 let colors = parseArgs(process.argv).c;
 colors = colors ? true : false;
-
+const logger = new Logger();
+const spinner = logger.startSpin("Analyzing the lasso bundle");
 if (input.length > 0) {
     input.map((fileName) => {
-        createBundle({ path: fileName, outputPath: 'lasso-analyze.js' }).createBundle;
+        try{
+            createBundle({ path: fileName, outputPath: 'lasso-analyze.js' }).createBundle;
+        }catch(e){
+            spinner.stop();
+            logger.error("Invalid file name or folder name");
+        }
+        
         lassoAnalyzer('lasso-analyze.js', { outputFile, colors });
     });
     outputFile = outputFile || 'lasso-analyze';
-    const startLog = `${borderX}` + `${outputFile}.html is created \n` +
-        `${borderX}`;
-    console.log(startLog);
+    spinner.stop();
+    logger.success(`${outputFile}.html is created`, true);
 } else {
-    console.log("No input provided\nUsage: lasso-analyzer < bundle.js >");
+    spinner.stop();
+    logger.error("Invalid input");
+    logger.info("Usage: lasso-analyzer < bundle.js >");
     return process.exit(1);
 }
